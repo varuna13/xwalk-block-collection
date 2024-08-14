41740c52ba813a7baffef5f498a4c1c55daf610c
Mihai Indreias <mindreias@adobe.com>
2024-08-14 11:55:36 +0200

    fix no-header option

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -14,22 +14,22 @@ export default async function decorate(block) {
   const table = document.createElement('table');
   const thead = document.createElement('thead');
   const tbody = document.createElement('tbody');
+  const header = !block.classList.contains('no-header');
 
   [...block.children].forEach((row, i) => {
     const tr = document.createElement('tr');
     moveInstrumentation(row, tr);
 
     [...row.children].forEach((cell) => {
-      const td = document.createElement(i === 0 ? 'th' : 'td');
+      const td = document.createElement(i === 0 && header ? 'th' : 'td');
 
       if (i === 0) td.setAttribute('scope', 'column');
       td.innerHTML = cell.innerHTML;
       tr.append(td);
     });
-    if (i === 0) thead.append(tr);
+    if (i === 0 && header) thead.append(tr);
     else tbody.append(tr);
   });
-  table.append(thead);
-  table.append(tbody);
+  table.append(thead, tbody);
   block.replaceChildren(table);
 }
